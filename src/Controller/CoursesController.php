3254bc12a50f646f4b3e525e151aba876957e63e
merciof <mercio.andrade@ufrpe.br>
2021-06-12 00:22:27 -0300

    add ResquestHandler component e view REST method

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -6,10 +6,27 @@ use Cake\Controller\Controller;
 
 class CoursesController extends Controller {
 
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('RequestHandler');
+    }
+
+
     public function index () {
-        $courses = $this->Courses->find('all')->toArray();
+        $courses = $this->Courses->find('all');
 
         $this->set('courses', $courses);
+        $this->set('_serialize', 'courses');
+    }
+
+    public function view($id)
+    {
+        $course = $this->Courses->get($id);
+        
+        $this->set('course', $course);
+        $this->set('_serialize', 'course');
+
     }
 
 }
